1d7905846bfdb79316b2cb60406924473f6a8ada
Eric Bower <me@erock.io>
2021-09-01 21:48:01 -0400

    add deploy script

---
+++ deploy.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+set -x
+
+$NODE --experimental-specifier-resolution=node --loader ts-node/esm scripts/scrape.ts
+$NODE --experimental-specifier-resolution=node --loader ts-node/esm scripts/process.ts
+$NODE --loader ts-node/esm scripts/html.ts
+rm -rf build
+./node_modules/.bin/svelte-kit build && $NODE scripts/postbuild.js
+gsutil -m rm -r gs://neovimcraft.com/*
+gsutil -m -h 'Cache-Control:private, max-age=0, no-transform' rsync -r ./build gs://neovimcraft.com
