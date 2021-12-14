64f87b9b24f58f5c74beded0c4946ce7ad53493c
Eric Bower <me@erock.io>
2021-12-14 00:52:37 -0500

    add flake

---
+++ flake.nix
@@ -0,0 +1,19 @@
+{
+  description = "development environment for robodux";
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+      in {
+        devShell = pkgs.mkShell {
+          buildInputs = [
+            pkgs.nodejs-14_x
+            pkgs.yarn
+          ];
+        };
+      });
+}
--- shell.nix
+++
@@ -1,9 +0,0 @@
-let pkgs = import <nixpkgs> {};
-
-in pkgs.mkShell rec {
-  name = "neovimcraft";
-
-  buildInputs = with pkgs; [
-    nodejs yarn google-cloud-sdk
-  ];
-}
